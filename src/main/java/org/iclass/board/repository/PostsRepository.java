f0d3e09fbd18cf4fe120ea5626b12dc5aa14a5f0
jincheol <sssm4719@gmail.com>
2024-10-20 23:05:09 +0900

    #2 게시판 DB 작업 기존 리액트에서 작업했던 query 검색 작업 이관(검색, 정렬, 페이지마다)

--- src/main/java/org/iclass/board/repository/PostsRepository.java
+++ src/main/java/org/iclass/board/repository/PostsRepository.java
@@ -2,11 +2,14 @@ package org.iclass.board.repository;
 
 import org.iclass.board.entity.PostsEntity;
 import org.iclass.board.entity.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PostsRepository extends JpaRepository<PostsEntity, Long> {
@@ -15,5 +18,11 @@ public interface PostsRepository extends JpaRepository<PostsEntity, Long> {
     // Post와 User를 조인해서 작성자 정보를 포함한 게시글 목록 가져오기
     @Query("SELECT p, u.username FROM PostsEntity p JOIN UserEntity u ON p.userId = u.userId ORDER BY p.createdAt DESC")
     List<Object[]> getPostsWithUsers();
+
+
+    @Query("SELECT p FROM PostsEntity p WHERE (:category = 'all' OR p.category = :category) AND p.title LIKE %:search% ORDER BY p.createdAt desc")
+    Page<PostsEntity> findByCategoryAndTitleContainingOrderByCreatedAtDesc(Pageable pageable, String category, String search);
+
+    Optional<PostsEntity> findByPostId(Long postId);
 }
 
